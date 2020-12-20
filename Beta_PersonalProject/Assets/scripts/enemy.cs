23ff7ae8283d7823a0d925083d19d8d6b2100679
NM6293 <71656485+NM6293@users.noreply.github.com>
2020-12-20 23:06:35 +0000

    Update enemy.cs

--- Beta_PersonalProject/Assets/scripts/enemy.cs
+++ Beta_PersonalProject/Assets/scripts/enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
-    public float speed;
+    private float speed = 25;
     private Rigidbody enemyRb;
     private GameObject player;
    private GameManager gameManager;
